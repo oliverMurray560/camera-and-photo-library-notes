a946ba83ef5be4075494260e26e029c465d175da
Oliver Murray <creeperman69@icloud.com>
2023-11-09 09:30:51 -0600

    Initial Commit

---
+++ camera and photo library notes/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  camera and photo library notes
+//
+//  Created by OLIVER MURRAY on 11/9/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ camera and photo library notesUITests/camera_and_photo_library_notesUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  camera_and_photo_library_notesUITestsLaunchTests.swift
+//  camera and photo library notesUITests
+//
+//  Created by OLIVER MURRAY on 11/9/23.
+//
+
+import XCTest
+
+final class camera_and_photo_library_notesUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
